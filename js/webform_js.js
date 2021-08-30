f52f919616d465818f11a6cb05467621b6fbd341
Lina <rladnfls0500@gmail.com>
2021-08-30 17:08:18 +0900

    id > class
    id는 한 페이지당 한개만 가능한 고유한 값이므로 브라우저에 따라 오류가 발생할 수 있다. > id 사용 지양하고 class로 수정해서 사용

--- js/webform_js.js
+++ js/webform_js.js
@@ -1,104 +1,58 @@
 //enable toogle popover
 $(function () {
-    $('[data-toggle="popover"]').popover()
-})
+    $('[data-toggle="popover"]').popover();
+    //modal
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
 
-//modal
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+    //add item
+    var id = 12;
 
-//add item
-var id = 12;
+    $(document).on("click", "#addBtn", function () {
+        var addTrText =
+            '<tr class = "' + id + '">' +
+            '<td style="text-align: center;"><i id="delete" class="fa fa-trash fa-border"></i></td>' +
+            '<td><input type="text" placeholder="새로 추가된 아이템" style="width:100%;"></input></td>' +
+            '<td></td>' +
+            '<td><input type="text" style="width:100%;"></input></td>' +
+            '<td><input type="text" style="width:100%;"></input></td>' +
+            '<td><input type="text" style="width:100%;"></input></td></tr>';
+        $("#supTab > tbody:last").append(addTrText);
 
-$(document).on("click", "#addBtn", function () {
-    var addTrText =
-        '<tr id = "' + id + '">' +
-        '<td style="text-align: center;"><i id="delete" class="fa fa-trash fa-border"></i></td>' +
-        '<td><input type="text" placeholder="새로 추가된 아이템" style="width:100%;"></input></td>' +
-        '<td></td>' +
-        '<td><input type="text" style="width:100%;"></input></td>' +
-        '<td><input type="text" style="width:100%;"></input></td>' +
-        '<td><input type="text" style="width:100%;"></input></td></tr>';
-    $("#supTab > tbody:last").append(addTrText);
+        id++;
+    });
 
-    id++;
-});
+    //edit item
+    $(document).on("click", "#edit", function () {
+        var trHtml = $(this).parent().parent();
 
-//edit item
-$(document).on("click", "#edit", function () {
-    var trHtml = $(this).parent().parent();
+        var tdHtml = trHtml.children();
 
-    var tdHtml = trHtml.children();
-
-    var idVal = trHtml.attr('id');
-    var nameHtml = tdHtml.eq(1).text();
-    var supplyHtml = tdHtml.eq(3).text();
-    var sellingHtml = tdHtml.eq(4).text();
-    var vendorHtml = tdHtml.eq(5).text();
-
-    var editable =
-        '<td style="text-align: center;">' + idVal + '</td>' +
-        '<td><input type="text" value="' + nameHtml + '" style="width:100%;"></td>' +
-        '<td><i id="save" class="fa fa-pencil fa-border"></i></td>' +
-        '<td><input type="text" value="' + supplyHtml + '" style="width:100%;"></td>' +
-        '<td><input type="text" value="' + sellingHtml + '" style="width:100%;"></td>' +
-        '<td><input type="text" value="' + vendorHtml + '" style="width:100%;"></td>';
-
-    $(this).parent().parent().html(editable);
-});
-
-//save changes
-$(document).on("click", "#save", function () {
-    var trHtml = $(this).parent().parent();
-
-    var tdHtml = trHtml.children();
-
-    var idStr = trHtml.attr('id');
-
-    var strName = tdHtml.eq(1).children().val();
-    var strSupply = tdHtml.eq(3).children().val();
-    var strSelling = tdHtml.eq(4).children().val();
-    var strVendor = tdHtml.eq(5).children().val();
-
-    var saveChange =
-        '<td style="text-align:center;">' + idStr + '</td>' +
-        '<td><a href="#">' + strName + '</a></td>' +
-        '<td><i id="edit" class="fa fa-pencil fa-border"></i></td>' +
-        '<td>' + strSupply + '</td>' +
-        '<td>' + strSelling + '</td>' +
-        '<td>' + strVendor + '</td>';
-
-    $(this).parent().parent().html(saveChange);
-});
-
-//edit all 일괄 수정
-$(document).on("click", "#editAll", function () {
-    for (var i = 1; i < 12; i++) {
-        var tdHtml = $("#" + i).children();
-
+        var idVal = trHtml.attr('class');
         var nameHtml = tdHtml.eq(1).text();
         var supplyHtml = tdHtml.eq(3).text();
         var sellingHtml = tdHtml.eq(4).text();
         var vendorHtml = tdHtml.eq(5).text();
 
         var editable =
-            '<td style="text-align: center;">' + i + '</td>' +
+            '<td style="text-align: center;">' + idVal + '</td>' +
             '<td><input type="text" value="' + nameHtml + '" style="width:100%;"></td>' +
             '<td><i id="save" class="fa fa-pencil fa-border"></i></td>' +
             '<td><input type="text" value="' + supplyHtml + '" style="width:100%;"></td>' +
             '<td><input type="text" value="' + sellingHtml + '" style="width:100%;"></td>' +
             '<td><input type="text" value="' + vendorHtml + '" style="width:100%;"></td>';
 
-        $("#" + i).html(editable);
-    }
-    $(this).attr('id', 'saveAll');
-});
+        $(this).parent().parent().html(editable);
+    });
 
-//save All
-$(document).on("click", "#saveAll", function () {
-    for (var i = 1; i < 12; i++) {
-        var tdHtml = $("#" + i).children();
+    //save changes
+    $(document).on("click", "#save", function () {
+        var trHtml = $(this).parent().parent();
+
+        var tdHtml = trHtml.children();
+
+        var idStr = trHtml.attr('class');
 
         var strName = tdHtml.eq(1).children().val();
         var strSupply = tdHtml.eq(3).children().val();
@@ -106,23 +60,69 @@ $(document).on("click", "#saveAll", function () {
         var strVendor = tdHtml.eq(5).children().val();
 
         var saveChange =
-            '<td style="text-align:center;">' + i + '</td>' +
+            '<td style="text-align:center;">' + idStr + '</td>' +
             '<td><a href="#">' + strName + '</a></td>' +
             '<td><i id="edit" class="fa fa-pencil fa-border"></i></td>' +
             '<td>' + strSupply + '</td>' +
             '<td>' + strSelling + '</td>' +
             '<td>' + strVendor + '</td>';
 
-        $("#" + i).html(saveChange);
-    }
-    $(this).attr('id', 'editAll');
-});
+        $(this).parent().parent().html(saveChange);
+    });
 
-//delete item
-$(document).on("click", "#delete", function () {
-    var trHtml = $(this)
-        .parent()
-        .parent();
+    //edit all 일괄 수정
+    $(document).on("click", "#editAll", function () {
+        for (var i = 1; i < 12; i++) {
+            var tdHtml = $("." + i).children();
 
-    trHtml.remove(); //delete tr tag
-});
+            var nameHtml = tdHtml.eq(1).text();
+            var supplyHtml = tdHtml.eq(3).text();
+            var sellingHtml = tdHtml.eq(4).text();
+            var vendorHtml = tdHtml.eq(5).text();
+
+            var editable =
+                '<td style="text-align: center;">' + i + '</td>' +
+                '<td><input type="text" value="' + nameHtml + '" style="width:100%;"></td>' +
+                '<td><i id="save" class="fa fa-pencil fa-border"></i></td>' +
+                '<td><input type="text" value="' + supplyHtml + '" style="width:100%;"></td>' +
+                '<td><input type="text" value="' + sellingHtml + '" style="width:100%;"></td>' +
+                '<td><input type="text" value="' + vendorHtml + '" style="width:100%;"></td>';
+
+            $("." + i).html(editable);
+        }
+        $(this).attr('id', 'saveAll');
+    });
+
+    //save All
+    $(document).on("click", "#saveAll", function () {
+        for (var i = 1; i < 12; i++) {
+            var tdHtml = $("." + i).children();
+
+            var strName = tdHtml.eq(1).children().val();
+            var strSupply = tdHtml.eq(3).children().val();
+            var strSelling = tdHtml.eq(4).children().val();
+            var strVendor = tdHtml.eq(5).children().val();
+
+            var saveChange =
+                '<td style="text-align:center;">' + i + '</td>' +
+                '<td><a href="#">' + strName + '</a></td>' +
+                '<td><i id="edit" class="fa fa-pencil fa-border"></i></td>' +
+                '<td>' + strSupply + '</td>' +
+                '<td>' + strSelling + '</td>' +
+                '<td>' + strVendor + '</td>';
+
+            $("." + i).html(saveChange);
+        }
+        $(this).attr('id', 'editAll');
+    });
+
+    //delete item
+    $(document).on("click", "#delete", function () {
+        var trHtml = $(this)
+            .parent()
+            .parent();
+
+        trHtml.remove(); //delete tr tag
+    });
+})
+
